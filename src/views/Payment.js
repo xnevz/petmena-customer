de4883d3f656ffcb7b22753cf27c27e5870be0bc
younes khenifer <86888662+YunsGeek@users.noreply.github.com>
2022-08-08 21:13:31 +0100

    added payment & payment sent

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -3,23 +3,29 @@ import React, {useState} from 'react';
 import {
   Box,
   Button,
+  Center,
   Checkbox,
   CheckIcon,
   Flex,
-  FormControl,
   Input,
-  ScrollView,
   Select,
   Text,
-  WarningOutlineIcon,
+  ScrollView,
 } from 'native-base';
 import PageHeader from '../components/PageHeader';
 import {useCheck} from '../customHooks/uiHooks';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
+
 export default function Payment() {
   const [cardType, setCardType] = useState('VISA');
-  const [checked, setChecked] = useState(true);
+  const [paymentSent, setPaymentSent] = useState(false);
   const accept_terms = useCheck();
+
+  // payment sent component
+  if (paymentSent) return <PaymentSent setPaymentSent={setPaymentSent} />;
+
   return (
     <Box flex={1} bg="gray.50">
       {/* header */}
@@ -120,12 +126,104 @@ export default function Payment() {
               bg="amber.500"
               borderRadius={10}
               shadow={1}
-              _text={{fontSize: 18, fontWeight: 'medium'}}>
+              _text={{fontSize: 18, fontWeight: 'medium'}}
+              onPress={() => setPaymentSent(true)}>
               Confirm
             </Button>
           </ScrollView>
         </Box>
       </Box>
+    </Box>
+  );
+}
+
+function PaymentSent({setPaymentSent}) {
+  const navigation = useNavigation();
+  return (
+    <Box flex={1} bg="gray.50">
+      {/* header */}
+      <PageHeader
+        label="PAYMENT"
+        iconColor="#000"
+        textColor="#000"
+        onBackPress={() => navigation.goBack()}
+      />
+
+      <ScrollView flex={1} mt="10px" px="10px">
+        <Center>
+          {/* check icon */}
+          <Ionicons name="md-checkmark-circle" size={150} color="#5F67EC" />
+
+          <Text fontSize="lg" color="black" mt="20px">
+            Payment Sent Successfully
+          </Text>
+        </Center>
+
+        {/* payment infos */}
+        <Box
+          mt="50px"
+          rounded="xl"
+          borderColor="gray.200"
+          shadow={1}
+          p="10px"
+          px="20px"
+          w="full"
+          bg="white">
+          {/* titles */}
+          <Flex direction="row" align="center">
+            <Text fontSize="sm" flex={2} color="gray.500">
+              Item
+            </Text>
+            <Text fontSize="sm" flex={2} color="gray.500">
+              Duration
+            </Text>
+            <Text fontSize="sm" flex={2} color="gray.500">
+              Time
+            </Text>
+            <Text fontSize="sm" flex={1} color="gray.500">
+              Price
+            </Text>
+          </Flex>
+          {/* divider */}
+          <Box w="full" h="2px" bg="teal.600" my="5px" />
+
+          {/* data */}
+          <Flex direction="row" align="flex-start" minH="100px">
+            <Text fontSize="sm" flex={2} color="gray.500">
+              Item name test text here BeTMENA
+            </Text>
+            <Text fontSize="sm" flex={2} color="gray.500">
+              60 min
+            </Text>
+            <Text fontSize="sm" flex={2} color="gray.500">
+              15-5-2022 15:00
+            </Text>
+            <Text fontSize="sm" flex={1} color="gray.500">
+              $50.00
+            </Text>
+          </Flex>
+
+          {/* divider */}
+          <Box w="full" h="2px" bg="teal.600" my="5px" />
+
+          <Flex direction="row" align="center" justify="flex-end">
+            <Text fontSize="lg" color="gray.500">
+              Total $50.00
+            </Text>
+          </Flex>
+        </Box>
+
+        {/* done btn */}
+        <Button
+          mt="50px"
+          bg="amber.500"
+          borderRadius={10}
+          shadow={1}
+          _text={{fontSize: 18, fontWeight: 'medium'}}
+          onPress={() => setPaymentSent(false)}>
+          Done
+        </Button>
+      </ScrollView>
     </Box>
   );
 }
@@ -148,4 +246,5 @@ function CustomInput({label, w, placeholder, value, onChangeText, ...props}) {
     </Box>
   );
 }
+
 const styles = StyleSheet.create({});
